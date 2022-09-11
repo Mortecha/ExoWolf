77e6993b9563d048828d8d9c320e8d00620c3ae5
Sam Keightley <sam.keightley@gmail.com>
2022-09-12 00:04:42 +1000

    mouse look working

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,16 +1,34 @@
 extends Spatial
 
+var ray_origin = Vector3()
+var ray_target = Vector3()
+var ray_length = 2000
+var mouse_position
+var intersection
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var player = $ExoWolf as KinematicBody
+#onready var navmap = $Navigation
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	#OS.window_fullscreen = true
 
+func _physics_process(delta):
+	var space_state = get_world().direct_space_state
+	var mouse_position = get_viewport().get_mouse_position()
+	ray_origin = $CameraPivot/Camera.project_ray_origin(mouse_position)
+	ray_target = ray_origin + $CameraPivot/Camera.project_ray_normal(mouse_position) * ray_length
+	var intersection = space_state.intersect_ray(ray_origin, ray_target)
+	if not intersection.empty():
+		var pos = intersection.position
+		player.look_at(Vector3(pos.x, pos.y, pos.z), Vector3.UP)
+	
+#	if is_instance_valid(player):
+#		mouse_position = get_viewport().get_mouse_position()	
+#		ray_origin = $CameraPivot/Camera.project_ray_origin(mouse_position)
+#		ray_target = ray_origin + $CameraPivot/Camera.project_ray_normal(mouse_position) * ray_length
+#		intersection = get_world().direct_space_state.intersect_ray(ray_origin, ray_target, [], 8)	
+#		if not intersection.empty():
+#			var target_vector = Vector3(intersection.position.x, player.translation.y, intersection.position.z)
+#			player.look_at(target_vector, Vector3.UP)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
