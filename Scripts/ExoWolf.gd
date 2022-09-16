4e2ca34d3923209c83571c517b032eda3b3fac9a
Sam Keightley <sam.keightley@gmail.com>
2022-09-16 11:43:45 +1000

    Implemented custom mouse look ray cast + incorporated tilt rotation
    This will need further work to move as one would expect based on inputs

--- Scripts/ExoWolf.gd
+++ Scripts/ExoWolf.gd
@@ -7,12 +7,25 @@ const MAX_SPEED = 40
 const MAX_ALTITUDE = 60
 const ACC = 2
 const DEC = 4
-const TILT_COEF = 4 # The max amount of tilt, Higher value less tilt  
-const TILT_RESP = 4 # The responsiveness of the tilting
+const TILT_COEF = 4 		# The max amount of tilt, Higher value less tilt  
+const TILT_RESP = 4 		# The responsiveness of the tilting
+const ROT_SPD = 4 			# Speed of rotation to point at mouse
+const RAY_LENGTH = 2000
+
+@export var camera : Camera3D
 
 # Movement
 var dir : Vector3 = Vector3()
 var acceleration : float
+
+# Rotation
+var space_state : PhysicsDirectSpaceState3D
+var mouse_position : Vector2
+var rayOrigin : Vector3
+var rayEnd : Vector3
+var parameters : PhysicsRayQueryParameters3D
+var intersection : Dictionary
+var to_angle : float
 
 func _ready():
 	pass
@@ -35,10 +48,19 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	# Tilt based on movement
-	rotation.x = lerp_angle(rotation.x, dir.z / TILT_COEF, delta * TILT_RESP)
-	rotation.z = lerp_angle(rotation.z, -dir.x / TILT_COEF, delta * TILT_RESP)
+	rotation.x = lerp_angle(rotation.x, dir.z / TILT_COEF, TILT_RESP * delta)
+	rotation.z = lerp_angle(rotation.z, -dir.x / TILT_COEF, TILT_RESP * delta)
 	
 	# Use this to figure out how to rotate y
-	#rotation.y = lerp_angle(rotation.y, atan2(-dir.x, -dir.z), delta * TILT_RESP)
+	#rotation.y = lerp_angle(rotation.y, atan2(-dir.x, -dir.z), TILT_RESP * delta)
 	
+	space_state = get_world_3d().direct_space_state
+	mouse_position = get_viewport().get_mouse_position()
+	rayOrigin = camera.project_ray_origin(mouse_position)
+	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * RAY_LENGTH
+	parameters = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd)
+	intersection = space_state.intersect_ray(parameters)
+	if not intersection.is_empty():
+		to_angle = atan2(-intersection.position.x, -intersection.position.z)
+		rotation.y = lerp_angle(rotation.y, to_angle, TILT_RESP * delta)
 
