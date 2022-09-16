f9f6553a564b0b0f1012f3b54a481f29f9a3d2cc
Sam Keightley <sam.keightley@gmail.com>
2022-09-17 00:11:29 +1000

    Updates to player movement + Godot 4 Beta 1 Upgrade

--- Scripts/ExoWolf.gd
+++ Scripts/ExoWolf.gd
@@ -6,7 +6,7 @@ extends CharacterBody3D
 const MAX_SPEED = 40
 const MAX_ALTITUDE = 60
 const ACC = 2
-const DEC = 4
+const DEC = .5
 const TILT_COEF = 4 		# The max amount of tilt, Higher value less tilt  
 const TILT_RESP = 4 		# The responsiveness of the tilting
 const ROT_SPD = 4 			# Speed of rotation to point at mouse
@@ -15,7 +15,7 @@ const RAY_LENGTH = 2000
 @export var camera : Camera3D
 
 # Movement
-var dir : Vector3 = Vector3()
+var input_vector : Vector3 = Vector3()
 var acceleration : float
 
 # Rotation
@@ -30,30 +30,32 @@ var to_angle : float
 func _ready():
 	pass
 
-func _physics_process(delta):	
-	dir.x = Input.get_axis(&"strafe_left", &"strafe_right")
-	dir.y = Input.get_axis(&"decrease_throttle", &"increase_throttle")
-	dir.z = Input.get_axis(&"move_forwards", &"move_backwards")
+func _physics_process(delta):
+	rotate_towards_mouse(delta)
+	
+	input_vector.x = Input.get_axis(&"strafe_left", &"strafe_right")
+	input_vector.y = Input.get_axis(&"decrease_throttle", &"increase_throttle")
+	input_vector.z = Input.get_axis(&"move_forwards", &"move_backwards")
 
 	# Limit the input to a length of 1. length_squared is faster to check.
-	if dir.length_squared() > 1: dir /= dir.length()
-	acceleration = ACC if dir.dot(velocity) > 0 else DEC
-	velocity = velocity.lerp(dir * MAX_SPEED, acceleration * delta)
-
-	# Altitude limit check
+	if input_vector.length_squared() > 1: input_vector /= input_vector.length()
+	acceleration = ACC if input_vector.dot(velocity) > 0 else DEC
+	
+	var current_rotation = rotation
+	rotation = Vector3(0, rotation.y, 0)
+	translate(input_vector)
+	rotation = current_rotation
+	
+#	# Altitude limit check
 	if transform.origin.y > MAX_ALTITUDE:
 		velocity.y = 0
 		transform.origin.y = MAX_ALTITUDE
-		
-	move_and_slide()
 	
 	# Tilt based on movement
-	rotation.x = lerp_angle(rotation.x, dir.z / TILT_COEF, TILT_RESP * delta)
-	rotation.z = lerp_angle(rotation.z, -dir.x / TILT_COEF, TILT_RESP * delta)
-	
-	# Use this to figure out how to rotate y
-	#rotation.y = lerp_angle(rotation.y, atan2(-dir.x, -dir.z), TILT_RESP * delta)
-	
+	rotation.x = lerp_angle(rotation.x, input_vector.z / TILT_COEF, TILT_RESP * delta)
+	rotation.z = lerp_angle(rotation.z, -input_vector.x / TILT_COEF, TILT_RESP * delta)
+
+func rotate_towards_mouse(delta):
 	space_state = get_world_3d().direct_space_state
 	mouse_position = get_viewport().get_mouse_position()
 	rayOrigin = camera.project_ray_origin(mouse_position)
@@ -61,6 +63,5 @@ func _physics_process(delta):
 	parameters = PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd)
 	intersection = space_state.intersect_ray(parameters)
 	if not intersection.is_empty():
-		to_angle = atan2(-intersection.position.x, -intersection.position.z)
-		rotation.y = lerp_angle(rotation.y, to_angle, TILT_RESP * delta)
+		rotation.y = lerp_angle(rotation.y, atan2(-intersection.position.x, -intersection.position.z), 1)
 
