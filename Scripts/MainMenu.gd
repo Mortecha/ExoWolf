b5bc07f0d29b16af2f79eeae9d4e78a81ae7567d
Sam Keightley <sam.keightley@gmail.com>
2022-09-18 03:17:03 +1000

    updates to file names + level transition + audio + land texture

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -3,8 +3,11 @@ extends Control
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
+func _process(delta):
+	pass
+
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Scenes/Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
 func _on_OptionsButton_pressed():
 	var options = load("res://Menus/Options.tscn").instance()
