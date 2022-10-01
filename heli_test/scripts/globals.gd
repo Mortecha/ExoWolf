8e059f8a38425254a00e4c0fba6a22c69430acc6
Sam Keightley <sam.keightley@gmail.com>
2022-10-02 01:43:42 +1000

    updates to menus

--- heli_test/scripts/globals.gd
+++ heli_test/scripts/globals.gd
@@ -27,10 +27,11 @@ var freelook : bool = false
 
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, Vector2(1280, 720), ui_scaling)
-	
-	update_mouse_sensitivity(mouse_sensitivity)
+	pass
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+#	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, Vector2(1280, 720), ui_scaling)
+#
+#	update_mouse_sensitivity(mouse_sensitivity)
 
 
 func _process(delta) -> void:
--- menus/start_screen.gd
+++ menus/start_screen.gd
@@ -8,14 +8,11 @@ onready var start_btn = $MainMenu/MarginContainer/VBoxContainer/StartGameBtn
 func _ready():
 	start_btn.grab_focus()
 
-func _on_StartGameBtn_pressed():
-# warning-ignore:return_value_discarded
+func _on_start_pressed():
 	get_tree().change_scene(level)
 
-
-func _on_SettingsBtn_pressed():
+func _on_settings_pressed():
 	settings_menu.popup_centered()
 
-
-func _on_ExitBtn_pressed():
+func _on_quit_pressed():
 	get_tree().quit()
