927eed06267db3c5fdb939a5a76feb61918cc6b9
Sam Keightley <sam.keightley@gmail.com>
2022-10-02 05:10:31 +1000

    further improvements to menus

--- menus/briefings/level_1_briefing/level_1_briefing.gd
+++ menus/briefings/level_1_briefing/level_1_briefing.gd
@@ -6,8 +6,8 @@ export(String, FILE) var level
 func _ready():
 	pass
 
-func _on_start_pressed():
-	get_tree().change_scene(level)
-
 func _on_main_menu_pressed():
 	get_tree().change_scene(main_menu)
+
+func _on_start_mission_pressed():
+	get_tree().change_scene(level)
--- menus/components/pause_menu/pause_menu.gd
+++ menus/components/pause_menu/pause_menu.gd
@@ -1,4 +1,7 @@
-extends Control
+extends CanvasLayer
+
+export(String, FILE) var main_menu
+export(String, FILE) var level
 
 var is_paused = false setget set_paused
 
@@ -13,14 +16,18 @@ func set_paused(value):
 	visible = is_paused
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if is_paused == true else Input.MOUSE_MODE_CAPTURED)
 
-func _on_ResumeBtn_pressed():
+func _on_return_pressed():
 	self.is_paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-func _on_SettingsBtn_pressed():
+func _on_restart_mission_pressed():
+	get_tree().change_scene(level)
+	
+func _on_settings_pressed():
 	settings_menu.popup_centered()
 
+func _on_main_menu_pressed():
+	get_tree().change_scene(main_menu)
 
-func _on_QuitBtn_pressed():
+func _on_quit_to_desktop_pressed():
 	get_tree().quit()
-
