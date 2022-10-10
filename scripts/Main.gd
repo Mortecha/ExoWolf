5429935936227a11d166538d581c005823b4b3fa
Sam Keightley <sam.keightley@gmail.com>
2022-10-11 01:59:25 +1000

    turret now points to mouse cursor

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -53,8 +53,13 @@ func _physics_process(delta):
 	time_delta = delta
 	cam_movement()
 	player_mouse_look()
+	player_nose_turret_to_target()
 	update_compass(player.rotation_degrees.y)
 	update_minimap(player.rotation_degrees.y)
+	
+	if Input.is_action_pressed("fire_weapon"):
+		pass
+		#gun_controller.shoot()
 
 func cam_movement():
 	mouse_player_interpolation()
@@ -80,7 +85,10 @@ func player_mouse_look():
 		var diff = look_target - player.transform.origin
 		if diff.x > 0.1 or diff.x < 0.1 and diff.z > 0.1 or diff.z < 0.1:
 			player.look_at(look_target, Vector3.UP)
-			
+
+func player_nose_turret_to_target():
+	player.get_child(3).set_target(look_target)
+	
 func update_compass(var angle : float):
 	# Angle gets multiplied by 2 so it can counts half direction between two directions (i.e. NE/NW/SW/SE)
 	# Angle * 2 - offset
