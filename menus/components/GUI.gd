241f9d892fb71805edd8bc5efdcac842bf16293d
Sam Keightley <sam.keightley@gmail.com>
2022-10-30 21:08:00 +1000

    overlay works

--- menus/components/GUI.gd
+++ menus/components/GUI.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 onready var pause_menu = $PauseMenu
-onready var tab_menu_overlay = $TabMenuOverlay
+#onready var tab_menu_overlay = $TabMenuOverlay
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,10 +10,8 @@ func _ready():
 func _unhandled_input(event):
 		
 	if event.is_action_pressed("pause"):
-		if not tab_menu_overlay.is_paused:
-			pause_menu.is_paused = !pause_menu.is_paused
-
+		pause_menu.is_paused = !pause_menu.is_paused
+			
 	if event.is_action_pressed("tab"):
-		if not pause_menu.is_paused:
-			tab_menu_overlay.is_paused = !tab_menu_overlay.is_paused
+		pass
 		
--- menus/components/mission_complete_overlay/mission_complete_overlay.gd
+++ menus/components/mission_complete_overlay/mission_complete_overlay.gd
@@ -7,19 +7,25 @@ onready var settings_menu = $SettingsMenu
 func _ready():
 	hide()
 
+
 func set_paused(value):
 	is_paused = value
 	get_tree().paused = value
 	visible = is_paused
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if is_paused == true else Input.MOUSE_MODE_CAPTURED)
 	
-func _on_resume_pressed():
+func _on_ResumeBtn_pressed():
 	self.is_paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_SettingsBtn_pressed():
 	settings_menu.popup_centered()
 
-func _on_quit_pressed():
+
+func _on_QuitBtn_pressed():
 	get_tree().quit()
 
+
+
+func _on_start_pressed():
+	pass # Replace with function body.
