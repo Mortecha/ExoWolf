f12dedee1ad3e1aa5a8626b54830d49f42dd1b22
Sam Keightley <sam.keightley@gmail.com>
2023-01-03 00:40:05 +1000

    Further updates to get 4.10 working

--- src/save/save.gd
+++ src/save/save.gd
@@ -8,29 +8,30 @@ func _ready():
 	load_data()
 
 func load_data():
-	var file = File.new()
-	if not file.file_exists(SAVEFILE):
-		game_data = {
-			"fullscreen_on": false,
-			"vsync_on": false,
-			"display_fps": false,
-			"max_fps": 0,
-			"bloom_on": false,
-			"brightness": 1,
-			"master_vol": -10,
-			"music_vol": -10,
-			"sfx_vol": -10,
-			"fov": 70,
-			"mouse_sens": .1,
-		}
+	if not FileAccess.file_exists(SAVEFILE):
+		create_new_file()
 		save_data()
-	file.open(SAVEFILE, File.READ)
-	game_data = file.get_var()
-	file.close()
+
+	FileAccess.open(SAVEFILE, FileAccess.READ)
+	#game_data = file.get_var()
+	
+func create_new_file():
+	var file = FileAccess.open(SAVEFILE, FileAccess.WRITE)
+	game_data = {
+		"fullscreen_on": false,
+		"vsync_on": false,
+		"display_fps": false,
+		"max_fps": 0,
+		"bloom_on": false,
+		"brightness": 1,
+		"master_vol": -10,
+		"music_vol": -10,
+		"sfx_vol": -10,
+		"fov": 70,
+		"mouse_sens": .1,
+	}
 	
 func save_data():
-	var file = File.new()
-	file.open(SAVEFILE, File.WRITE)
+	var file = FileAccess.open(SAVEFILE, FileAccess.WRITE)
 	file.store_var(game_data)
-	file.close()
 		
